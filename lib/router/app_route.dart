6ce4c5cca10efa09ff7587058c85a912e4c54572
Ángel Rubén Valdeolmos Carmona <angel.valdeolmos@outlook.es>
2023-10-05 14:46:37 +0200

    clear

--- lib/router/app_route.dart
+++ lib/router/app_route.dart
@@ -2,20 +2,14 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:smartfoodinsight_app/common/utils/utis.dart';
 import 'package:smartfoodinsight_app/features/features.dart';
-import 'package:smartfoodinsight_app/router/app_router_listenable.dart';
 
 final goRouterProvider = Provider<GoRouter>((ref) {
-  final goRouterNotifier = ref.read(goRouterNotifierProvider);
-  return GoRouter(
-      initialLocation: AppSettings.home,
-      refreshListenable: goRouterNotifier,
-      routes: [
-        GoRoute(
-            path: AppSettings.signup,
-            builder: (context, state) => const RegisterPage()),
-        GoRoute(
-            path: AppSettings.home,
-            builder: (context, state) => const LoginPage()),
-        //GoRoute(path: home, builder: (context, state) => const SideMenu()),
-      ]);
+  return GoRouter(initialLocation: AppSettings.home, routes: [
+    GoRoute(
+        path: AppSettings.signup,
+        builder: (context, state) => const RegisterPage()),
+    GoRoute(
+        path: AppSettings.home, builder: (context, state) => const LoginPage()),
+    //GoRoute(path: home, builder: (context, state) => const SideMenu()),
+  ]);
 });
