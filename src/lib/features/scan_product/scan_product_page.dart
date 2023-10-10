b940f1926e3ffc36fddebf8dfb79bf978df79e21
Ángel Rubén Valdeolmos Carmona <angel.valdeolmos@outlook.es>
2023-10-10 16:01:50 +0200

    mobile scanner

--- src/lib/features/scan_product/scan_product_page.dart
+++ src/lib/features/scan_product/scan_product_page.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
-class ScanProductPage extends StatelessWidget {
+class ScanProductPage extends ConsumerWidget {
   const ScanProductPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const Text('Scan');
+  Widget build(BuildContext context, WidgetRef ref) {
+    MobileScannerController mobileScannerController = MobileScannerController();
+
+    return MobileScanner(
+      onDetect: (barcodeCapture) => onBarCodeCapture,
+      controller: mobileScannerController,
+    );
+  }
+
+  void onBarCodeCapture(BarcodeCapture barcodeCapture) {
+    String aaa = barcodeCapture.raw;
   }
 }
