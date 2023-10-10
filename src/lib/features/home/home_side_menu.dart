e1438b18fbb018587a1ebc183fc5195236f95217
Ángel Rubén Valdeolmos Carmona <angel.valdeolmos@outlook.es>
2023-10-10 14:19:48 +0200

    logout

--- src/lib/features/home/home_side_menu.dart
+++ src/lib/features/home/home_side_menu.dart
@@ -1,24 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:smartfoodinsight_app/common/providers/providers.dart';
 import 'package:smartfoodinsight_app/common/utils/app_settings.dart';
+import 'package:smartfoodinsight_app/common/widgets/widgets.dart';
 import 'package:smartfoodinsight_app/models/models.dart';
 
-class HomeSideMenu extends StatefulWidget {
+class HomeSideMenu extends ConsumerStatefulWidget {
   const HomeSideMenu({super.key});
 
   @override
-  State<HomeSideMenu> createState() => _HomeSideMenu();
+  HomeSideMenuState createState() => HomeSideMenuState();
 }
 
-class _HomeSideMenu extends State<HomeSideMenu> {
+class HomeSideMenuState extends ConsumerState<HomeSideMenu> {
   int navDrawerIndex = 0;
   GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
+    final authNotifier = ref.read(authNotifierProvider.notifier);
+
     final hasNotch = MediaQuery.viewPaddingOf(context).top > 35;
     return Scaffold(
       key: scaffoldKey,
       appBar: AppBar(title: Text(menuItems[navDrawerIndex].title)),
       drawer: NavigationDrawer(
+          elevation: 1,
           selectedIndex: navDrawerIndex,
           onDestinationSelected: (value) => onDestinationSelected(value),
           children: [
@@ -28,7 +35,13 @@ class _HomeSideMenu extends State<HomeSideMenu> {
             const Divider(),
             ...menuItems.sublist(0, 2).map((item) =>
                 NavigationDrawerDestination(
-                    icon: Icon(item.icon), label: Text(item.title)))
+                    icon: Icon(item.icon), label: Text(item.title))),
+            const Divider(),
+            Padding(
+                padding: EdgeInsets.fromLTRB(29, hasNotch ? 16 : 20, 16, 10),
+                child: GeneralElevatedButton(
+                    onPressed: () async => authNotifier.logoutAsync(),
+                    child: const Text('Cerrar sesión'))),
           ]),
       body: menuItems[navDrawerIndex].page,
     );
