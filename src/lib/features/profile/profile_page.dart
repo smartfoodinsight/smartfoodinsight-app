cce3826e4e2949dcf7fc95591f128dca386571ec
Ángel Rubén Valdeolmos Carmona <angel.valdeolmos@outlook.es>
2023-11-08 12:03:49 +0100

    design profile #6

--- src/lib/features/profile/profile_page.dart
+++ src/lib/features/profile/profile_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 import 'package:smartfoodinsight_app/common/widgets/widgets.dart';
 import 'package:smartfoodinsight_app/common/extensions/extensions.dart';
@@ -11,6 +12,7 @@ class ProfilePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final authNotifer = ref.read(authNotifierProvider.notifier);
+    final cameraGalleryService = ref.read(cameraGalleryServiceProvider);
 
     return SafeArea(
       child: Scaffold(
@@ -20,9 +22,62 @@ class ProfilePage extends ConsumerWidget {
               padding: const EdgeInsets.all(30),
               child: Column(
                 children: [
-                  const CircleAvatar(
-                    radius: 70,
-                    backgroundImage: AssetImage('assets/images/foodwaste.png'),
+                  GestureDetector(
+                    onTap: () => {
+                      showModalBottomSheet<void>(
+                          context: context,
+                          isScrollControlled: true,
+                          builder: (BuildContext context) {
+                            return Padding(
+                              padding: MediaQuery.viewInsetsOf(context),
+                              child: SizedBox(
+                                height: 150,
+                                width: double.infinity,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    ListTile(
+                                        title: Text(context.loc.camera),
+                                        leading:
+                                            const Icon(Icons.camera_alt_sharp),
+                                        onTap: () async {
+                                          context.pop();
+                                          final imagePath =
+                                              await cameraGalleryService
+                                                  .takePhotoAsync();
+                                          if (imagePath == null) return;
+                                        }),
+                                    ListTile(
+                                        title: Text(context.loc.gallery),
+                                        leading: const Icon(Icons.image),
+                                        onTap: () async {
+                                          context.pop();
+                                          final imagePath =
+                                              await cameraGalleryService
+                                                  .selectPhotoAsync();
+                                          if (imagePath == null) return;
+                                        }),
+                                  ],
+                                ),
+                              ),
+                            );
+                          })
+                    },
+                    child: const CircleAvatar(
+                      radius: 70,
+                      backgroundImage:
+                          AssetImage('assets/images/foodwaste.png'),
+                      child: Stack(children: [
+                        Align(
+                            alignment: Alignment.bottomRight,
+                            child: CircleAvatar(
+                                radius: 18,
+                                backgroundColor: Colors.grey,
+                                child: Icon(Icons.camera_alt,
+                                    color: Colors.black) // change this children
+                                ))
+                      ]),
+                    ),
                   ),
                   const SizedBox(height: 16),
                   NormalTextFormField(
@@ -35,6 +90,9 @@ class ProfilePage extends ConsumerWidget {
                       label: context.loc.email,
                       textInputType: TextInputType.emailAddress,
                       icon: Icons.email),
+                  const SizedBox(height: 16),
+                  GeneralElevatedButton(
+                      onPressed: null, child: Text(context.loc.save)),
                   const SizedBox(height: 16),
                   const Divider(),
                   ListTile(
