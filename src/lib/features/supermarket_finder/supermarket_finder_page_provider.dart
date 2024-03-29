e4023de59be3c5c7ebbe820c6531e50da7c53e8c
Ángel Rubén Valdeolmos Carmona <angel.valdeolmos@outlook.es>
2024-03-29 11:48:01 +0100

    clean

--- src/lib/features/supermarket_finder/supermarket_finder_page_provider.dart
+++ src/lib/features/supermarket_finder/supermarket_finder_page_provider.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:smartfoodinsight_app/common/providers/providers.dart';
